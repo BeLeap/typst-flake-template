ebd0be4fd96ef4dd38f2d52ea2dca2d98056c450
BeLeap <beleap@beleap.dev>
2025-11-05 01:57:28 +0900

    try use flake template

--- flake.nix
+++ flake.nix
@@ -1,43 +1,13 @@
 {
-  description = "Typst flake template";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    press.url = "github:RossSmyth/press";
-    typst-live.url = "github:ItsEthra/typst-live";
-  };
+  description = "Simple typst template";
 
   outputs =
+    { ... }:
     {
-      self,
-      nixpkgs,
-      flake-utils,
-      press,
-      typst-live,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import press) ];
+      templates = {
+        default = {
+          path = ./template;
         };
-      in
-      rec {
-        packages.default = pkgs.buildTypstDocument {
-          name = "main";
-          src = ./.;
-        };
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ packages.default ];
-          packages = with pkgs; [
-            tinymist
-            typstyle
-            (typst-live.packages.${system}.default)
-          ];
-        };
-      }
-    );
+      };
+    };
 }
